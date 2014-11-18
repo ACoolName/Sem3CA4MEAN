5a92ee2fa1b96d9ab7fbd7d9ea4541bce902037b
sallas <tbc105@hotmail.com>
2014-11-18 13:58:04 +0100

    Added Wiki model and mongoose deletion of object in e2e test

--- test/e2e-tests/scenarios.js
+++ test/e2e-tests/scenarios.js
@@ -3,7 +3,8 @@
 global.TEST_DATABASE = "mongodb://localhost/TestDataBase_MEAN";
 var db = require('../../server/model/db');
 var mongoose = require("mongoose");
-var wiki = require("Wiki");
+var Wiki = mongoose.model("Wiki");
+
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
@@ -18,13 +19,21 @@ describe('my app', function () {
 
     describe('wiki', function () {
 
-        beforeEach(function () {
+        beforeEach(function (done) {
             browser.get('#/wiki');
+            Wiki.remove({}, function () {
+                var array = [
+
+                ];
+                Wiki.create(array, function (err) {
+                    done();
+                });
+            });
         });
 
 
         it('should render view1 when user navigates to /wiki', function () {
-            var wikis = element.all(by.repeater('item in items'));
+            var wikis = element.all(by.repeater('wiki in wikis'));
             expect(wikis.count()).toEqual(2);
         });
 
